f17226363ca48338f8e695ff5d036d31e00f9841
Med El-Moussaoui Tijani <30555101+assuom44@users.noreply.github.com>
2023-12-02 15:53:34 +0100

    Update get_network_dataset.R

--- R/get_network_dataset.R
+++ R/get_network_dataset.R
@@ -1,5 +1,17 @@
 # Funciton to load network dataset
 
 get_network_dataset <- function (dataset = DS) {
+  # Create an empty graph object
+  graph <- graph.empty()
+  
+  # Load dataset
   network_data <- read.table(DS, header = FALSE)
+  
+  # Add edges to the graph
+  graph <- graph_from_data_frame(network_data, directed = FALSE)
+
+  return (graph)
 }
+
+
+
