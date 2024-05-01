f7837fc6752a35ba5bc7312d4b0074863b62e11c
Med El-Moussaoui Tijani <30555101+assuom44@users.noreply.github.com>
2024-05-02 00:07:57 +0100

    Update clean_final_rules.R
    reveiwing the file structure

--- R/clean_final_rules.R
+++ R/clean_final_rules.R
@@ -1,24 +1,63 @@
-# Function the clean the preprocessed rules (non redundant and significant rules)
+#' Clean Final Rules
+#'
+#' Cleans the preprocessed rules by removing redundant and significant rules.
+#'
+#' @description This function takes a set of preprocessed rules, removes redundant
+#' and significant rules, and returns a cleaned vector of rules.
+#'
+#' @param final_rules A dataframe containing the preprocessed rules.
+#'
+#' @return A cleaned vector of rules.
+#'
+#' @examples
+#' clean_final_rules(final_rules)
+#' @import stringr
+#' @export
 
-clean_final_rules <- function (final_rules) {
+clean_final_rules <- function(final_rules) {
   ## Cleaning generated rules for further processing :
-  rules_subset <- final_rules
   
   # Convert S4 object to data frame
-  df <- as(rules_subset, "data.frame")
+  df <- as.data.frame(final_rules)
   
-  # Removing extra sign from rules structure
+  # Removing extra characters from rules structure
   clean_df <- gsub('[{}=>"]', "", df$rules)
-  clean_df <- gsub("  ", ",", clean_df)
-  clean_df <- gsub(" ", ",", clean_df)
+  clean_df <- gsub("\\s+", ",", clean_df)
   
-  # Conversion rules components to numeric values
+  # Splitting rules into components and converting to numeric values
   values <- lapply(strsplit(clean_df, ","), as.numeric)
-
-  # Sorting result
-  clean_df <- lapply(values, sort)
-  # Remove duplicates
-  vec <- unique (clean_df)
-
-  retunr (vec)
+  
+  # Sorting and removing duplicates
+  vec <- unique(lapply(values, function(x) sort(unique(x))))
+  
+  return(vec)
 }
+
+
+
+
+
+
+# # Function the clean the preprocessed rules (non redundant and significant rules)
+# clean_final_rules <- function (final_rules) {
+#   ## Cleaning generated rules for further processing :
+#  rules_subset <- final_rules
+  
+#   # Convert S4 object to data frame
+#   df <- as(rules_subset, "data.frame")
+  
+#   # Removing extra sign from rules structure
+#   clean_df <- gsub('[{}=>"]', "", df$rules)
+#   clean_df <- gsub("  ", ",", clean_df)
+#   clean_df <- gsub(" ", ",", clean_df)
+  
+#   # Conversion rules components to numeric values
+#   values <- lapply(strsplit(clean_df, ","), as.numeric)
+
+#   # Sorting result
+#   clean_df <- lapply(values, sort)
+#   # Remove duplicates
+#   vec <- unique (clean_df)
+
+#   retunr (vec)
+# }
