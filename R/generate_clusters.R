8c245f81dd26c331cb537eff5877c980fdaddd93
Med El-Moussaoui Tijani <30555101+assuom44@users.noreply.github.com>
2024-05-02 00:10:51 +0100

    Update generate_clusters.R
    Reveiwing the file structure

--- R/generate_clusters.R
+++ R/generate_clusters.R
@@ -1,38 +1,87 @@
-# This function tends to generate potential clusters based on the preprocesed rules
+#' Generate Clusters
+#'
+#' This function generates potential clusters based on preprocessed rules.
+#'
+#' @description This function takes a vector of preprocessed rules, combines elements 
+#' starting with the same value, groups segments by the starting value, sorts elements 
+#' within each segment, and returns potential clusters.
+#'
+#' @param vec A vector of preprocessed rules.
+#' @param minSupp The minimum support threshold for generating clusters.
+#' @param minConf The minimum confidence threshold for generating clusters.
+#'
+#' @return A list of potential clusters.
+#'
+#' @examples
+#' generate_clusters(vec, minSupp = 0.5, minConf = 0.6)
+#' @import Fct_CleanOverlappingTrans
+#' @export
 
-generate_clusters <- function (vec, minSupp, minConf) {
-  # Init result container
-  result <- vec
-  
+generate_clusters <- function(vec, minSupp, minConf) {
   # Combine elements starting with the same value and remove duplicates
-  combined_result <- lapply(result, function(vec) {
-    unique(unlist(vec))
-  })
+  combined_result <- lapply(vec, function(v) unique(unlist(v)))
   
   # Group segments by the starting value
   grouped_result <- split(combined_result, sapply(combined_result, `[`, 1))
   
   # Sort elements within each segment
-  sorted_result <- lapply(grouped_result, function(vec) {
-    sort(unique(unlist(vec)))
-  })
+  sorted_result <- lapply(grouped_result, function(v) sort(unique(unlist(v))))
   
-  best_support <- minSupp
-  best_confidence <- minConf
-  
+  # Call external function to generate clusters
   genFileLog <- TRUE # Activate generated spool (FALSE if not)
-  debugModeOption <- FALSE # Disable debuging mode (TRUE if activated option)
-  clusters <- Fct_CleanOverlappingTrans( sorted_result, 
+  debugModeOption <- FALSE # Disable debug mode (TRUE if activated option)
+  clusters <- Fct_CleanOverlappingTrans(sorted_result, 
                                          genFileLog = genFileLog,  
                                          debugMode  = debugModeOption) 
-
-  # Display obtained clusters:
+  
+  # Display obtained clusters
   if (genFileLog) {
-    cat ("Total Clusters: ", length(clusters))
+    cat("Total Clusters: ", length(clusters))
     for (i in 1:length(clusters)) {
-        cat("\n  Set ", i, ":", paste(clusters[[i]], collapse = " "))
+      cat("\n  Set ", i, ":", paste(clusters[[i]], collapse = " "))
     }
   }
-
-  return (clusters)
+  
+  return(clusters)
 }
+
+                          
+                          
+
+# This function tends to generate potential clusters based on the preprocesed rules
+#generate_clusters <- function (vec, minSupp, minConf) {
+#  # Init result container
+#  result <- vec
+  
+#  # Combine elements starting with the same value and remove duplicates
+#  combined_result <- lapply(result, function(vec) {
+#    unique(unlist(vec))
+#  })
+  
+#  # Group segments by the starting value
+#  grouped_result <- split(combined_result, sapply(combined_result, `[`, 1))
+  
+#  # Sort elements within each segment
+#  sorted_result <- lapply(grouped_result, function(vec) {
+#    sort(unique(unlist(vec)))
+#  })
+  
+#  best_support <- minSupp
+#  best_confidence <- minConf
+  
+#  genFileLog <- TRUE # Activate generated spool (FALSE if not)
+#  debugModeOption <- FALSE # Disable debuging mode (TRUE if activated option)
+#  clusters <- Fct_CleanOverlappingTrans( sorted_result, 
+#                                         genFileLog = genFileLog,  
+#                                         debugMode  = debugModeOption) 
+
+# # Display obtained clusters:
+#  if (genFileLog) {
+#    cat ("Total Clusters: ", length(clusters))
+#    for (i in 1:length(clusters)) {
+#        cat("\n  Set ", i, ":", paste(clusters[[i]], collapse = " "))
+#    }
+#  }
+
+#  return (clusters)
+#}
