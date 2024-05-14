1599ac6e89a05c94cb02565be438fd718306997a
assuom44 <me.elmoussaoui@gmail.com>
2024-05-14 21:51:33 +0100

    confirmed

--- R/clean_final_rules.R
+++ R/clean_final_rules.R
@@ -16,48 +16,24 @@
 
 clean_final_rules <- function(final_rules) {
   ## Cleaning generated rules for further processing :
-  
+
   # Convert S4 object to data frame
+  #df <- as(rules_subset, "data.frame")
   df <- as.data.frame(final_rules)
-  
+
   # Removing extra characters from rules structure
+  #   clean_df <- gsub('[{}=>"]', "", df$rules)
+  #   clean_df <- gsub("  ", ",", clean_df)
+  #   clean_df <- gsub(" ", ",", clean_df)
   clean_df <- gsub('[{}=>"]', "", df$rules)
   clean_df <- gsub("\\s+", ",", clean_df)
-  
+
   # Splitting rules into components and converting to numeric values
+  #values <- lapply(strsplit(clean_df, ","), as.numeric)
   values <- lapply(strsplit(clean_df, ","), as.numeric)
-  
+
   # Sorting and removing duplicates
   vec <- unique(lapply(values, function(x) sort(unique(x))))
-  
+
   return(vec)
 }
-
-
-
-
-
-
-# # Function the clean the preprocessed rules (non redundant and significant rules)
-# clean_final_rules <- function (final_rules) {
-#   ## Cleaning generated rules for further processing :
-#  rules_subset <- final_rules
-  
-#   # Convert S4 object to data frame
-#   df <- as(rules_subset, "data.frame")
-  
-#   # Removing extra sign from rules structure
-#   clean_df <- gsub('[{}=>"]', "", df$rules)
-#   clean_df <- gsub("  ", ",", clean_df)
-#   clean_df <- gsub(" ", ",", clean_df)
-  
-#   # Conversion rules components to numeric values
-#   values <- lapply(strsplit(clean_df, ","), as.numeric)
-
-#   # Sorting result
-#   clean_df <- lapply(values, sort)
-#   # Remove duplicates
-#   vec <- unique (clean_df)
-
-#   retunr (vec)
-# }
