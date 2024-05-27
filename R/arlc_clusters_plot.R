327b3ed37b18227101b171c30dc37b65952081b6
assuom44 <me.elmoussaoui@gmail.com>
2024-05-27 23:46:12 +0100

    plot function file 27May2024

--- R/arlc_clusters_plot.R
+++ R/arlc_clusters_plot.R
@@ -6,58 +6,63 @@
 #' @param graphLabel A character string for the graph label to be displayed in the title.
 #' @param clusters A list of clusters to highlight in the plot.
 #'
-#' @return No return value. The function produces a plot as a side effect.
+#' @return The function produces a plot as a side effect.
 #'
 #' @examples
 #' \dontrun{
-#' library(igraph)
-#' g <- make_ring(10)
-#' arlc_clusters_plot(g, "Example Graph", list(1:5, 6:10))
+#' arlc_clusters_plot(g$graph, g$graphLabel, c$Clusters)
 #' }
 #' @import igraph
 #' @export
 arlc_clusters_plot <- function(g, graphLabel, clusters) {
-  # Calculate the layout once
-  layout <- layout_with_fr(g)
-  cluster_colors <- rainbow(length(clusters))
+  # Display obtained clusters
+  cat ("Total Identified Clusters: ", length(clusters))
+  cat ("=========================  ")
+  for (i in 1:length(clusters)) {
+    cat("\n  Set ", i, ":", paste(clusters[[i]], collapse = " "))
+  }
+  cat ("=========================  ")
 
-  # Set up the plotting area to have two plots side-by-side
-  par(mfrow = c(1, 2))
+  if (vcount (g) <= 70)
+  {
+    # Calculate the layout once
+    layout <- layout_with_fr(g)
+    cluster_colors <- rainbow(length(clusters))
 
-  # Plot the original network
-  plot(g,
-       layout = layout,
-       vertex.color = "gray",
-       vertex.label.color = "black",
-       vertex.label.size = 0.5,
-       vertex.label.cex = 0.7,
-       vertex.label.dist = 0,
-       edge.arrow.size = 0.3,
-       edge.color = "gray",
-       main = "Initial")
+    # Set up the plotting area to have two plots side-by-side
+    par(mfrow = c(1, 2))
 
-  # Plot the network with clusters highlighted
-  plot(g,
-       layout = layout,
-       mark.groups = clusters,
-       vertex.color = "gray",
-       vertex.label.color = "black",
-       vertex.label.size = 0.5,
-       vertex.label.cex = 0.7,
-       vertex.label.dist = 0,
-       main = "ARLClustering",
-       edge.arrow.size = 0.3,
-       edge.color = "gray")
+    # Plot the original network
+    plot(g,
+         layout = layout,
+         vertex.color = "gray",
+         vertex.label.color = "black",
+         vertex.label.size = 0.5,
+         vertex.label.cex = 0.7,
+         vertex.label.dist = 0,
+         edge.arrow.size = 0.3,
+         edge.color = "gray",
+         main = "Initial")
 
-  # Add a legend to the second plot (optional)
-  #legend("bottomright", legend = 1:length(clusters), fill = cluster_colors, title = "Clusters", cex = 0.8, inset = 0.02)
-  legend("topright", legend = 1:length(clusters), fill = cluster_colors, title = "Clusters", cex = 0.8, inset = 0.02, bty = "n", pt.cex = 1.5)
+    # Plot the network with clusters highlighted
+    plot(g,
+         layout = layout,
+         mark.groups = clusters,
+         vertex.color = "gray",
+         vertex.label.color = "black",
+         vertex.label.size = 0.5,
+         vertex.label.cex = 0.7,
+         vertex.label.dist = 0,
+         main = "ARLClustering",
+         edge.arrow.size = 0.3,
+         edge.color = "gray")
 
-  # Reset to default single plot layout
-  par(mfrow = c(1, 1))
+    # Add a legend to the second plot (optional)
+    #legend("bottomright", legend = 1:length(clusters), fill = cluster_colors, title = "Clusters", cex = 0.8, inset = 0.02)
+    legend("topright", legend = 1:length(clusters), fill = cluster_colors, title = "Clusters", cex = 0.8, inset = 0.02, bty = "n", pt.cex = 1.5)
+
+    # Reset to default single plot layout
+    par(mfrow = c(1, 1))
+  }
 }
 
-# Example usage:
-# library(igraph)
-# g <- make_ring(10)
-# arlc_clusters_plot(g, "Example Graph", list(1:5, 6:10))
